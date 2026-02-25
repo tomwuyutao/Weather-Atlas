843138b72662dbaf171b7c51e154719aab7c1e40
Yutao Wu <tomwuyutao@gmail.com>
2026-02-25 10:30:10 +0000

    Initial Commit

---
+++ Weather/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  Weather
+//
+//  Created by Tom on 25/02/2026.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
